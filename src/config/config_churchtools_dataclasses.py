b9a4b413abc739d8f155ff63612724cce418807f
Jonathan Straub <j.f.straub@gmx.de>
2025-05-03 03:26:58 +0200

    add docs to churchtools config dataclasses
    Signed-off-by: Jonathan Straub <j.f.straub@gmx.de>

---
+++ src/config/config_churchtools_dataclasses.py
@@ -0,0 +1,113 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class ManageStreamBehaviorConf:
+    """
+    Dataclass configuring the ChurchTools fact for Stream Behavior
+
+    Attributes:
+
+    - *name*: Name of the Fact
+    - *yes_value*: Value equaling to "yes" (create a stream)
+    - *ignore_value*: Value equaling to "ignore" (don't touch this event)
+    - *default*: Default value to use when any other value is set
+    """
+    name: str
+    yes_value: str
+    ignore_value: str
+    default: str
+
+
+@dataclass
+class StreamVisibilityConf:
+    """
+    Dataclass configuring the ChurchTools fact for Stream Visibility
+
+    Attributes:
+
+    - *name*: Name of the Fact
+    - *visible_value*: Value for a public YouTube Stream
+    - *unlisted_value*: Value for an unlisted YouTube Stream
+    - *private_value*: Value for a private YouTube Stream
+    - *default*: Default value to use when any other value is set
+    """
+    name: str
+    visible_value: str
+    unlisted_value: str
+    private_value: str
+    default: str
+
+
+@dataclass
+class IncludeInCalConf:
+    """
+    Dataclass configuring the ChurchTools fact for including the stream link inside the calendar entry
+
+    Attributes:
+
+    - *name*: Name of the Fact
+    - *yes_value*: Value equaling to 'yes' / True (do include in calendar)
+    - *default*: Default behavior
+    """
+    name: str
+    yes_value: str
+    default: bool
+
+
+@dataclass
+class ShowOnHomepageConf:
+    """
+    Dataclass configuring the ChurchTools fact for showing the stream on the WordPress site
+
+    Attributes:
+
+    - *name*: Name of the Fact
+    - *yes_value*: Value equaling to 'yes' / True (do include in calendar)
+    - *default*: Default behavior
+    """
+    name: str
+    yes_value: str
+    default: bool
+
+
+@dataclass
+class ChurchToolsConf:
+    """
+    Datatclass holding configuration about ChurchTools
+
+    Attributes:
+
+    - *instance*: URL to your ChurchTools instance
+    - *token*: API Token string
+    - *days_to_load*: How many days to load in advance
+
+    - *speaker_service_name*: Title of the ChurchTools service name containing the speaker's name
+    - *thumbnail_name*: Name of the service attachment containing the streams Thumbnail
+    - *stream_attachment_name*: Name of the service attachment pointing to the YouTube stream
+
+    The other attributes are described in the documentation of the respective classes
+    """
+    instance: str
+    token: str
+    days_to_load: int
+
+    manage_stream_behavior_fact: ManageStreamBehaviorConf
+    stream_visibility_fact: StreamVisibilityConf
+    include_in_cal_fact: IncludeInCalConf
+    show_on_homepage_fact: ShowOnHomepageConf
+
+    speaker_service_name: str
+
+    thumbnail_name: str
+    stream_attachment_name: str
+
+
+@dataclass
+class Config:
+    """
+    CTLA Main Configuration
+    """
+    churchtools: ChurchToolsConf
+    youtube: None
+    wordpress: None
