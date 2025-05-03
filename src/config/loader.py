66e6235e9f5e85a805b8496876a2f3e87e0244cb
Jonathan Straub <j.f.straub@gmx.de>
2025-05-03 15:18:21 +0200

    load environment variables
    Signed-off-by: Jonathan Straub <j.f.straub@gmx.de>

--- src/config/loader.py
+++ src/config/loader.py
@@ -27,6 +27,27 @@ def combine_into(delta: dict, combined: dict) -> None:
             combined[k] = v
 
 
+def filter_none[T: dict](target: T) -> T:
+    """
+    Recursively filter `None`-values and empty dictionaries from dictionary target
+
+    :param target: The dictionary to operate on
+    :return: The new dictionary, with `None`-values filtered
+    """
+    result = {}
+    for k, v in target.items():
+        # Recurse into sub-dicts
+        if isinstance(v, dict):
+            v = filter_none(v)
+            # Skip now-empty dicts
+            if not v:
+                continue
+        # Filter out None-values
+        if v is not None:
+            result[k] = v
+    return result
+
+
 def _load_default_config() -> Config:
     with open(os.path.join(os.path.dirname(__file__), 'default_config.json'), 'r') as default_file:
         return json.load(default_file)
@@ -37,10 +58,34 @@ def _load_user_config() -> Config:
 
 
 def _load_env_config() -> Config:
-    for k, v in os.environ.items():
-        if not k.startswith('CTLA_'):
-            continue
-        # TODO
+    """
+    Load known environment variables into a Config dict and return it.
+
+    Values that are not set from environment variables will be stripped from the dictionary.
+    This function ensures proper types for the returned values.
+
+    :return: The config dict
+    """
+    # noinspection PyTypeChecker
+    raw_values: Config = {
+        'churchtools': {
+            'instance': os.getenv('CTLA_CT_INSTANCE'),
+            'token': os.getenv('CTLA_CT_TOKEN'),
+            'days_to_load': None,
+            'manage_stream_behavior_fact': None,
+            'stream_visibility_fact': None,
+            'include_in_cal_fact': None,
+            'show_on_homepage_fact': None,
+            'speaker_service_name': None,
+            'thumbnail_name': None,
+            'stream_attachment_name': None
+        },
+        'youtube': {},
+        'wordpress': {}
+    }
+    # Filter out undefined values (from when an environment variable was not set)
+    # to avoid overriding the other config sources
+    return filter_none(raw_values)
 
 
 def _load_cli_params() -> Config:
@@ -69,7 +114,8 @@ def load_config() -> tuple[ChurchToolsConf, None, None]:
     combine_into(_load_user_config(), config)
 
     # Load ENV parameters
-    combine_into(_load_env_config(), config)
+    envc = _load_env_config()
+    combine_into(envc, config)
 
     # Load CLI parameters
     combine_into(_load_cli_params(), config)
