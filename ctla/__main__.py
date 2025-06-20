a0d645784dc411ac193c0f8fc1be14fef82d4fe7
Jonathan Straub <j.f.straub@gmx.de>
2025-06-21 00:27:23 +0200

    youtube update logic
    Signed-off-by: Jonathan Straub <j.f.straub@gmx.de>

--- ctla/__main__.py
+++ ctla/__main__.py
@@ -5,7 +5,7 @@ import config
 import delete
 import setup
 import update
-from config import args
+from configs import args
 from ct.ChurchTools import ChurchTools
 from yt.YouTube import YouTube
 
@@ -17,3 +17,14 @@ config.load()
 
 ct = ChurchTools()
 yt = YouTube()
+
+events = setup.gather_event_info(ct, yt)
+events = (ev for ev in events if ev.category_id == 30)
+pprint.pprint(list(events))
+
+for event in events:
+    if event.wants_stream:
+        if not event.yt_broadcast:
+            update.create_youtube(ct, yt, event)
+
+        update.update_youtube(yt, event)  # TODO
--- ctla/update.py
+++ ctla/update.py
@@ -1,9 +1,10 @@
 """
 Update functions: create & update events, broadcasts, stuff
 """
-
+import datetime
 import logging
 
+import config
 from ct.ChurchTools import ChurchTools
 from data import Event
 from yt.YouTube import YouTube
@@ -14,7 +15,8 @@ log = logging.getLogger(__name__)
 
 def create_youtube(ct: ChurchTools, yt: YouTube, event: Event):
     """
-    Create a YouTube broadcast for the given event, attach it and upload the link to ChurchTools.
+    Create a YouTube broadcast for the given event and bind the stream id;
+    then attach it and upload the link to ChurchTools.
 
     This still requires `update_youtube()` to be called in order to set all attributes correctly
 
@@ -23,5 +25,39 @@ def create_youtube(ct: ChurchTools, yt: YouTube, event: Event):
     :param event: The event wanting a broadcast
     """
     bc: LiveBroadcast = yt.create_broadcast(event.title, event.start_time, event.yt_visibility)
+    bc = yt.bind_stream_to_broadcast(bc['id'], config.youtube['stream_key_id'])
     event.yt_broadcast = bc
     ct.set_stream_link(event, f'https://youtu.be/{bc['id']}')
+
+
+def update_youtube(yt: YouTube, ev: Event):
+    """
+    Update the information on YouTube to reflect the one given in ChurchTools.
+
+    Only updates when necessary.
+
+    :param yt: YouTube service instance
+    :param ev: The event to update information for
+    """
+    if not ev.yt_broadcast:
+        return
+    bc = ev.yt_broadcast
+    data = dict()
+
+    yt_title = ev.yt_title
+    if yt_title != bc['snippet']['title']:
+        data['title'] = yt_title
+
+    yt_desc = ev.yt_description
+    if yt_desc != bc['snippet']['title']:
+        data['desc'] = yt_desc
+    if ev.start_time != datetime.datetime.fromisoformat(bc['snippet']['scheduledStartTime']):
+        data['start'] = ev.start_time
+    if ev.end_time != datetime.datetime.fromisoformat(bc['snippet']['scheduledEndTime']):
+        data['end'] = ev.end_time
+    if ev.yt_visibility != bc['status']['privacyStatus']:
+        data['privacy'] = ev.yt_visibility
+
+    bc = yt.set_broadcast_info(bc, **data)
+    bc = yt.set_thumbnails(bc, ev.yt_thumbnail.url if ev.yt_thumbnail else config.youtube['thumbnail_uri'])
+    ev.yt_broadcast = bc
