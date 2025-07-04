a1a189788def53fe330d088071d4f8c5d7b5dd47
Jonathan Straub <j.f.straub@gmx.de>
2025-07-04 23:43:35 +0200

    maintenance: attribute doccomments for configs
    Signed-off-by: Jonathan Straub <j.f.straub@gmx.de>

--- ctla/configs/churchtools.py
+++ ctla/configs/churchtools.py
@@ -1,104 +1,90 @@
-from typing import TypedDict
+from typing import TypedDict, Optional
 
 
 class ManageStreamBehaviorConf(TypedDict):
     """
     Dataclass configuring the ChurchTools fact for Stream Behavior
-
-    Attributes:
-
-    - *name*: Name of the Fact
-    - *yes_value*: Value equaling to "yes" (create a stream)
-    - *ignore_value*: Value equaling to "ignore" (don't touch this event)
-    - *no_value*: Value equaling to "no" (don't create a stream, and delete existing streams)
-    - *default*: Default value to use when any other value is set
     """
+
     name: str
+    """Name of the Fact"""
     yes_value: str
+    """Value equaling to "yes" (create a stream)"""
     ignore_value: str
+    """Value equaling to "ignore" (don't touch this event)"""
     no_value: str
+    """Value equaling to "no" (don't create a stream, and delete existing streams)"""
     default: str
+    """Default value to use when any other value is set"""
 
 
 class StreamVisibilityConf(TypedDict):
     """
     Dataclass configuring the ChurchTools fact for Stream Visibility
-
-    Attributes:
-
-    - *name*: Name of the Fact
-    - *visible_value*: Value for a public YouTube Stream
-    - *unlisted_value*: Value for an unlisted YouTube Stream
-    - *private_value*: Value for a private YouTube Stream
-    - *default*: Default value to use when any other value is set
     """
+
     name: str
+    """Name of the Fact"""
     visible_value: str
+    """Value for a public YouTube Stream"""
     unlisted_value: str
+    """Value for an unlisted YouTube Stream"""
     private_value: str
+    """Value for a private YouTube Stream"""
     default: str
+    """Default value to use when any other value is set"""
 
 
 class IncludeInCalConf(TypedDict):
     """
     Dataclass configuring the ChurchTools fact for including the stream link inside the calendar entry
-
-    Attributes:
-
-    - *name*: Name of the Fact
-    - *yes_value*: Value equaling to 'yes' / True (do include in calendar)
-    - *no_value*: Value equaling to 'no' / False (don't include in calendar)
-    - *default*: Default behavior
     """
+
     name: str
+    """Name of the Fact"""
     yes_value: str
+    """Value equaling to 'yes' / True (do include in calendar)"""
     no_value: str
+    """Value equaling to 'no' / False (don't include in calendar)"""
     default: bool
+    """Default behavior"""
 
 
 class ShowOnHomepageConf(TypedDict):
     """
     Dataclass configuring the ChurchTools fact for showing the stream on the WordPress site
-
-    Attributes:
-
-    - *name*: Name of the Fact
-    - *yes_value*: Value equaling to 'yes' / True (do show on homepage)
-    - *no_value*: Value equaling to 'no' / False (don't show on homepage)
-    - *default*: Default behavior
     """
+
     name: str
+    """Name of the Fact"""
     yes_value: str
+    """Value equaling to 'yes' / True (do show on homepage)"""
     no_value: str
+    """Value equaling to 'no' / False (don't show on homepage)"""
     default: bool
+    """Default behavior"""
 
 
 class ChurchToolsConf(TypedDict):
     """
     Dataclass holding configuration about ChurchTools
-
-    Attributes:
-
-    - *instance*: URL to your ChurchTools instance
-    - *token*: API Token string
-    - *days_to_load*: How many days to load in advance
-
-    - *speaker_service_name*: Title of the ChurchTools service name containing the speaker's name
-    - *thumbnail_name*: Name of the service attachment containing the streams Thumbnail
-    - *stream_attachment_name*: Name of the service attachment pointing to the YouTube stream
-
-    The other attributes are described in the documentation of the respective classes
     """
+
     instance: str
+    """URL to your ChurchTools instance"""
     token: str
+    """API Token string"""
     days_to_load: int
+    """How many days to load in advance"""
 
     manage_stream_behavior_fact: ManageStreamBehaviorConf
     stream_visibility_fact: StreamVisibilityConf
-    include_in_cal_fact: IncludeInCalConf | None
-    show_on_homepage_fact: ShowOnHomepageConf | None
+    include_in_cal_fact: Optional[IncludeInCalConf]
+    show_on_homepage_fact: Optional[ShowOnHomepageConf]
 
-    speaker_service_name: str | None
-
-    thumbnail_name: str | None
+    speaker_service_name: Optional[str]
+    """Title of the ChurchTools service name containing the speaker's name"""
+    thumbnail_name: Optional[str]
+    """Name of the service attachment containing the streams Thumbnail"""
     stream_attachment_name: str
+    """Name of the service attachment pointing to the YouTube stream"""
--- ctla/configs/youtube.py
+++ ctla/configs/youtube.py
@@ -25,51 +25,49 @@ class YouTubeTemplateConf(TypedDict):
 class YouTubeBroadcastSettingsConf(TypedDict):
     """
     Dataclass holding technical settings for broadcasts
-
-    Attributes:
-
-    - `enable_monitor_stream`: Whether the monitoring stream should be enabled
-    - `broadcast_stream_delay_ms`: If there is a monitoring stream, how much delay should be added to the public stream
-    - `enable_embed`: Enable embedding
-    - `enable_dvr`: Enable DVR controls during the livestream
-    - `record_from_start`: Record the stream and publish it as video later
-    - `closed_captions_type`: Whether to provide closed captions
-    - `latency_preference`: The latency preference. either 'normal', 'low', or 'ultraLow'
-    - `enable_auto_start`: Whether to automatically "Go Live" when receiving data on the bound stream
-    - `enable_auto_stop`: Whether to automatically stop the broadcast around 1min after receiving data
     """
+
     enable_monitor_stream: bool
+    """Whether the monitoring stream should be enabled"""
     broadcast_stream_delay_ms: int
+    """If there is a monitoring stream, how much delay should be added to the public stream"""
     enable_embed: bool
+    """Enable embedding"""
     enable_dvr: bool
+    """Enable DVR controls during the livestream"""
     record_from_start: bool
+    """Record the stream and publish it as video later"""
     closed_captions_type: Literal['closedCaptionsDisabled', 'closedCaptionsHttpPost', 'closedCaptionsEmbedded']
+    """Whether to provide closed captions"""
     latency_preference: Literal['normal', 'low', 'ultraLow']
+    """The latency preference. either 'normal', 'low', or 'ultraLow'"""
     enable_auto_start: bool
+    """Whether to automatically "Go Live" when receiving data on the bound stream"""
     enable_auto_stop: bool
+    """Whether to automatically stop the broadcast around 1min after receiving data"""
 
 
 class YouTubeConf(TypedDict):
     """
     Dataclass holding configuration about the YouTube API connection
-
-    Attributes:
-
-    - `redirect_uri`: Redirection URI for the OAuth flow, including scheme and optional path.
-    - `redirect_port`: Port to listen on for OAuth redirects.
-    - `client_secrets_file`: Path to the file containing the Google API client secret
-    - `credentials_file`: Path where the app stores the API Tokens received from Google
-    - `templates`: Template configuration for YouTube title / description
-    - `thumbnail_uri`: URI to a default thumbnail to be set on all new streams.
-      Can be a path to a file, a `file://`-URI or web address
-    - `stream_key_id`: The identifier of the stream key to use (not the key itself!)
-    - `broadcast_settings`: Setting to apply to newly created broadcasts
     """
+
     redirect_url: str
+    """Redirection URI for the OAuth flow, including scheme and optional path."""
     redirect_port: int
+    """Port to listen on for OAuth redirects."""
     client_secrets_file: str
+    """Path to the file containing the Google API client secret"""
     credentials_file: str
+    """Path where the app stores the API Tokens received from Google"""
     templates: YouTubeTemplateConf
+    """Template configuration for YouTube title / description"""
     thumbnail_uri: str
+    """
+    URI to a default thumbnail to be set on all new streams.
+    Can be a path to a file, a `file://`-URI or web address
+    """
     stream_key_id: str
+    """The identifier of the stream key to use (not the key itself!)"""
     broadcast_settings: YouTubeBroadcastSettingsConf
+    """Setting to apply to newly created broadcasts"""
